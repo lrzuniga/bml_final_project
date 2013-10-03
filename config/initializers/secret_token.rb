15545a1262a12751b48d297f5aed61321a884e2b
lrzuniga <lrzuniga@gmail.com>
2013-10-03 18:15:53 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CurateMe::Application.config.secret_key_base = 'bd60f15204d0e2e706970529d0c16bef45b38eb07dff8d535fae3a3901e08125f9713460ac11fea6781893bdc0badf016466a4dc3512ed10b10abea41274a14b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CurateMe::Application.config.session_store :cookie_store, key: '_CurateMe_session'
